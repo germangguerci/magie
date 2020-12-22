c2b5dd2e4e7422c224b75a25cefa098e9f91d68d
Death97-byte <68304510+Death97-byte@users.noreply.github.com>
2020-12-22 15:35:02 -0300

    add cart

---
+++ src/components/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Cart = () => {
+
+    return (
+        <div>
+            <h1>Cart</h1>
+            
+        </div>
+    )
+}
+
+export default Cart;
+
+
+
+const fakeUsers = [
+    {
+        publicKey: "TEST-6669a518-2be6-41a7-9068-4226dbaf3661",
+        accessToken: "TEST-1294034537296050-122216-b6f75add24d60a1ee8d7d72a9f7b0953-184851111"
+    },
+];
+
+const apiKey = [
+    {
+        clientKey: "ck_9be9e390810677fc9eb7ae2c51c2533a9e58669e",
+        clientSecretKey: "cs_cfdc41e2cff98ba78c5820d13666b2e51cdb9a9c"
+    }
+];
