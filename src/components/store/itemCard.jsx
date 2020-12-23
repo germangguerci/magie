fd314ec2c5f21e69b594f8f3d465a0a22dfdf8b0
Zaba <Nzabattaro@gmail.com>
2020-12-23 15:03:52 -0300

    Catalogo update 0.1.0
    List all products
    Each product have his own card
    Responsive design

--- src/components/store/itemCard.jsx
+++ src/components/store/itemCard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './styles/itemcard.css'
+import { addToCart } from '../../redux/CartReducer/actions';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom'
+
 
 const ItemCard = ({product}) => {
+    const dispatch = useDispatch();
+
+    const handleAddToCart = e => {
+        console.log("E: ", e);
+        e.preventDefault();
+        dispatch(addToCart(product))
+    }
+
     return(
-        <div  className="container">
-            <p>carta</p>
-            <span>{product.name}</span>
-            <span>{product.id}</span>
+        <div className="Product">
+            <div className="contenedorImagen">
+                <img src={product.images[0].src}  className="clip-ellipse" alt=""/>
+            </div>
+                <h2>{product.name}</h2>
+                <p>Precio: ${product.price}</p>
+                <button onClick={e => handleAddToCart(e) } >Add To Cart</button>
         </div>        
     )
 }
