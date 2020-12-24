f8819ee9ed6948d30e7d679cf851fa6ff2b7e1b4
Germangguerci <germangguerci@gmail.com>
2020-12-23 22:57:37 -0300

    Token saved in LocalStorage

--- src/components/Customer/GetToken.js
+++ src/components/Customer/GetToken.js
@@ -6,9 +6,11 @@ const contraseña = "m&!2iKaXn#gnnjVY"
 export const GetToken = () => {
     axios.post(`http://magiosbootcamp.ml/wp-json/jwt-auth/v1/token`, {username:usuario, password:contraseña})
       .then((response) => {
-        console.log(response);
+       var token = response.data.data.token;
+       window.localStorage.setItem("tokenkey", token);
         return (<p>Succes</p>)
       })
+      .then(() => {console.log("Succes: ", window.localStorage.getItem("tokenkey"))})
       .catch((error) => {console.log("Create customer error: ", error); return (<p>Error</p>);});
       return null;
   }
