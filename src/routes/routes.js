f3c2760542dbf90e406cb05a3ca321f3ea1fb802
AgustinJaime99 <71357291+AgustinJaime99@users.noreply.github.com>
2020-12-27 15:19:44 -0300

    Navbar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import {Route} from 'react-router-dom';
 import Store from '../components/store/store';
 import {CreateCustomer} from '../components/CreateCustomer';
 import Cart from '../components/Cart/Cart';
+import Navbar from '../components/Navbar/Navbar'
 
 
 const routes = () => {
@@ -10,6 +11,7 @@ const routes = () => {
             <Route path="/store" component={Store} />
             <Route path="/createcustomer" component={CreateCustomer} />
             <Route path="/cart" component={Cart} />
+            <Route path="/" component={Navbar} />
         </>
     )
     
